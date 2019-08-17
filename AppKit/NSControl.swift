5505d6e9b09f03813aa1180bd1126cd637d3b094
Pavel Sharanda <edvaef@gmail.com>
2019-08-17 21:35:34 +0300

    NSButton clicked

---
+++ AppKit/NSControl.swift
@@ -0,0 +1,24 @@
+//
+//  NSButton.swift
+//  Jetpack-iOS
+//
+//  Created by Pavel Sharanda on 8/17/19.
+//  Copyright © 2019 Jetpack. All rights reserved.
+//
+
+import Cocoa
+
+extension JetpackExtensions where Base: NSButton {
+    
+    public var clicked: Observable<Void> {
+        return jx_makeTargetActionSubject(key: #function, setup: { base, target, action in
+            base.target = target
+            base.action = action
+        }, cleanup: { base, _, _ in
+            base.target = nil
+            base.action = nil
+        }, getter: { _ in
+            ()
+        }).asObservable
+    }
+}
